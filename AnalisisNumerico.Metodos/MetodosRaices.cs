e0712dc92d5c398b32a8de9b239d2d9a66783165
ES94 <e.stradella94@outlook.com>
2017-08-08 16:31:33 -0300

    Commit Nº 1 - 08/08/2018

---
+++ AnalisisNumerico.Metodos/MetodosRaices.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisNumerico.Metodos
+{
+    public class MetodosRaices
+    {
+        private decimal ResultadosRaices { get; set; }
+
+        private int TopeIteraciones = 100;
+
+        private decimal ErrorRelativo { get; set; }
+
+        private bool OperacionFallida { get; set; }
+
+        public double Funcion(double x)
+        {
+            return Math.Pow((x - 3), 2) - 1;
+        }
+
+        private double? Biseccion(double Xi, double Xf)
+        {
+            double? resultado = 0;
+            int contadorIteraciones = 0;
+            OperacionFallida = false;
+
+            while ((Xi != Xf) && (contadorIteraciones <= TopeIteraciones))
+            {
+                double puntoMedio = (Xi + Xf) / 2;
+
+                contadorIteraciones++;
+
+                if (Funcion(puntoMedio) == 0)
+                {
+                    resultado = Funcion(puntoMedio);
+
+                    break;
+                }
+                else if ((Funcion(Xi) * Funcion(Xf)) < 0)
+                {
+                    Xf = puntoMedio;
+                }
+                else
+                {
+                    Xi = puntoMedio;
+                }
+
+            }
+
+            if (Xi == Xf)
+            {
+                resultado = null;
+            }
+
+            if (contadorIteraciones == TopeIteraciones)
+            {
+                OperacionFallida = true;
+            }
+
+            return resultado;
+        }
+
+        private void ReglaFalsa()
+        {
+
+        }
+
+        private void Newton()
+        {
+
+        }
+
+        private void Secante()
+        {
+
+        }
+    }
+}
---
+++ AnalisisNumerico.UI/UIPrincipal.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace AnalisisNumerico.UI
+{
+    partial class UIPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.raícesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sistemasDeEcuacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regresiónEInterpolaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.integraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.métodosCerradosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.métodosAbiertosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bisecciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reglaFalsaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newtonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.secanteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.raícesToolStripMenuItem,
+            this.sistemasDeEcuacionesToolStripMenuItem,
+            this.regresiónEInterpolaciónToolStripMenuItem,
+            this.integraciónToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // raícesToolStripMenuItem
+            // 
+            this.raícesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.métodosCerradosToolStripMenuItem,
+            this.métodosAbiertosToolStripMenuItem});
+            this.raícesToolStripMenuItem.Name = "raícesToolStripMenuItem";
+            this.raícesToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.raícesToolStripMenuItem.Text = "Raíces";
+            // 
+            // sistemasDeEcuacionesToolStripMenuItem
+            // 
+            this.sistemasDeEcuacionesToolStripMenuItem.Name = "sistemasDeEcuacionesToolStripMenuItem";
+            this.sistemasDeEcuacionesToolStripMenuItem.Size = new System.Drawing.Size(143, 20);
+            this.sistemasDeEcuacionesToolStripMenuItem.Text = "Sistemas de ecuaciones";
+            // 
+            // regresiónEInterpolaciónToolStripMenuItem
+            // 
+            this.regresiónEInterpolaciónToolStripMenuItem.Name = "regresiónEInterpolaciónToolStripMenuItem";
+            this.regresiónEInterpolaciónToolStripMenuItem.Size = new System.Drawing.Size(153, 20);
+            this.regresiónEInterpolaciónToolStripMenuItem.Text = "Regresión e Interpolación";
+            // 
+            // integraciónToolStripMenuItem
+            // 
+            this.integraciónToolStripMenuItem.Name = "integraciónToolStripMenuItem";
+            this.integraciónToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.integraciónToolStripMenuItem.Text = "Integración";
+            // 
+            // métodosCerradosToolStripMenuItem
+            // 
+            this.métodosCerradosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bisecciónToolStripMenuItem,
+            this.reglaFalsaToolStripMenuItem});
+            this.métodosCerradosToolStripMenuItem.Name = "métodosCerradosToolStripMenuItem";
+            this.métodosCerradosToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.métodosCerradosToolStripMenuItem.Text = "Métodos cerrados";
+            // 
+            // métodosAbiertosToolStripMenuItem
+            // 
+            this.métodosAbiertosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newtonToolStripMenuItem,
+            this.secanteToolStripMenuItem});
+            this.métodosAbiertosToolStripMenuItem.Name = "métodosAbiertosToolStripMenuItem";
+            this.métodosAbiertosToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.métodosAbiertosToolStripMenuItem.Text = "Métodos abiertos";
+            // 
+            // bisecciónToolStripMenuItem
+            // 
+            this.bisecciónToolStripMenuItem.Name = "bisecciónToolStripMenuItem";
+            this.bisecciónToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bisecciónToolStripMenuItem.Text = "Bisección";
+            // 
+            // reglaFalsaToolStripMenuItem
+            // 
+            this.reglaFalsaToolStripMenuItem.Name = "reglaFalsaToolStripMenuItem";
+            this.reglaFalsaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.reglaFalsaToolStripMenuItem.Text = "Regla Falsa";
+            // 
+            // newtonToolStripMenuItem
+            // 
+            this.newtonToolStripMenuItem.Name = "newtonToolStripMenuItem";
+            this.newtonToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newtonToolStripMenuItem.Text = "Newton";
+            // 
+            // secanteToolStripMenuItem
+            // 
+            this.secanteToolStripMenuItem.Name = "secanteToolStripMenuItem";
+            this.secanteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.secanteToolStripMenuItem.Text = "Secante";
+            // 
+            // UIPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "UIPrincipal";
+            this.Text = "Análisis Numérico";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem raícesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sistemasDeEcuacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem regresiónEInterpolaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem integraciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem métodosCerradosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bisecciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reglaFalsaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem métodosAbiertosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newtonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem secanteToolStripMenuItem;
+    }
+}
+
