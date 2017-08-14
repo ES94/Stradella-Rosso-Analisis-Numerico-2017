d36cc6166c37435a772b71eb091c813dc4546e33
ES94 <e.stradella94@outlook.com>
2017-08-14 20:11:49 -0300

    Commit Nº 2 - 14/08/2017

---
+++ AnalisisNumerico.UI/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnalisisNumerico.UI
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ItmRaices_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void itmSistEcuaciones_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void itmRegInter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void itmIntegracion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ItmSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
