3e62729ed3a4e4334ee0eca5ddd82decad3639f4
ES94 <e.stradella94@outlook.com>
2017-09-25 21:56:31 -0300

    Commit Nº 19 - 25/09/2017

--- AnalisisNumerico.Metodos/MetodosSistEcuaciones.cs
+++ AnalisisNumerico.Metodos/MetodosSistEcuaciones.cs
@@ -13,5 +13,71 @@ namespace AnalisisNumerico.Metodos
 {
     public static class MetodosSistEcuaciones
     {
+        /// <summary>
+        /// Obtiene un vector de los resultados de un sistema de ecuaciones a partir de la construcción y reducción de una matriz
+        /// obtenida de los coeficientes del sistema.
+        /// </summary>
+        /// <param name="n">Orden de la matriz cuadrada de coeficientes.</param>
+        /// <param name="vectorCoeficientes">Vector de todos los coeficientes ordenados, empezando desde los coeficientes de la primera
+        /// ecuación hasta los coeficientes de la última ecuación.</param>
+        public static List<double> GaussJordan(List<List<double>> matrizCoeficientes)
+        {
+            List<double> vectorResultado = new List<double>(matrizCoeficientes.Count);
+
+            for (int columna = 0; columna < matrizCoeficientes.Count; columna++)
+            {
+                List<double> eqNormalizada = matrizCoeficientes[columna];
+
+                /*
+                 * Normalizar ecuación a utilizar en la columna actual.
+                 */
+                for (int i = 0; i < eqNormalizada.Count; i++)
+                {
+                    eqNormalizada[i] /= eqNormalizada.Max();
+                }
+
+                for (int fila = 0; fila < matrizCoeficientes.Count; fila++)
+                {
+                    if (columna != fila)
+                    {
+                        double coeficiente = matrizCoeficientes[fila][columna];
+                        List<double> ecuacionActual = matrizCoeficientes[fila];
+                        List<double> eqNormalizadaPorCoef = eqNormalizada;
+
+                        /*
+                         * Se multiplica la ecuación normalizada por el coeficiente actual.
+                         */
+                        for (int i = 0; i < eqNormalizada.Count; i++)
+                        {
+                            eqNormalizadaPorCoef[i] = eqNormalizada[i] * coeficiente;
+                        }
+
+                        /*
+                         * A la ecuación en la fila actual se le resta el producto la ecuación normalizada por el coeficiente.
+                         */
+                        for (int i = 0; i < ecuacionActual.Count; i++)
+                        {
+                            ecuacionActual[i] -= eqNormalizadaPorCoef[i];
+                        }
+                    }
+                }
+            }
+
+            /*
+             * Se rellena al vector con los valores de resultado. Las posiciones de los coeficientes indican a qué variable pertenecen.
+             */
+            for (int fila = 0; fila < matrizCoeficientes.Count; fila++)
+            {
+                for (int columna = 0; columna < matrizCoeficientes.Count; columna++)
+                {
+                    if (fila == columna)
+                    {
+                        vectorResultado.Add(matrizCoeficientes[fila][columna]);
+                    }
+                }
+            }
+
+            return vectorResultado;
+        }
     }
 }
